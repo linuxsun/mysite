5b1c946682ba2d45a3af45e9690eb55cbd7471d8
Your Name <you@example.com>
2020-01-13 09:21:09 +0800

    new add

--- mysite/settings.example.py
+++ mysite/settings.example.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't2-f)rwv+xfqa=m@#@a@=5)^0p0q9fbjf)9w%uc)%)pdj1*%n7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
